da25461e6afaa5eb70836b30688200601abe457d
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-10-21 12:00:19 +0300

    Alustettu lopputyö

---
+++ alehtola/src/test/java/com/alehtola/alehtola/AlehtolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alehtola.alehtola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlehtolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
