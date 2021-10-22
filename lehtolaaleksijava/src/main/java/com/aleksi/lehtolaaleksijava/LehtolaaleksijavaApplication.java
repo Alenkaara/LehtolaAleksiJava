001a52e222a291e5f99772ccbfb685470341ff12
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-10-22 11:37:48 +0300

    uusi

---
+++ lehtolaaleksijava/src/main/java/com/aleksi/lehtolaaleksijava/LehtolaaleksijavaApplication.java
@@ -0,0 +1,13 @@
+package com.aleksi.lehtolaaleksijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehtolaaleksijavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LehtolaaleksijavaApplication.class, args);
+	}
+
+}
---
+++ lehtolaaleksijava/src/test/java/com/aleksi/lehtolaaleksijava/LehtolaaleksijavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksi.lehtolaaleksijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LehtolaaleksijavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
